09ff47c2d67bff26db725a6aedfb55f03c8d7e4f
Patryk Wnukowski <wnuq@ymail.com>
2023-07-26 06:50:51 +0200

    create interface module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { InterfaceModule } from "./modules/interface/interface.module";
 
 @NgModule({
     declarations: [
         AppComponent,
+        NavbarComponent
     ],
 
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+
+        InterfaceModule
     ],
 
     providers: [],
 
     bootstrap: [
         AppComponent
-    ]
+    ],
 })
 
 export class AppModule {};
